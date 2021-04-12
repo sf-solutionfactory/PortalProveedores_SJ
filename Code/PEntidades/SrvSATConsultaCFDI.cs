04bdc0b346794d6f463711a7386bc33f89c32913
Rogelio Sánchez Gómez <36237248+sf-rogeliosanchez@users.noreply.github.com>
2021-04-12 13:05:05 -0500

    Error HTTP.sys 2

--- Code/PEntidades/SrvSATConsultaCFDI.cs
+++ Code/PEntidades/SrvSATConsultaCFDI.cs
@@ -12,117 +12,117 @@ namespace PEntidades.SrvSATConsultaCFDI
 {
     using System.Net;//ADD SF RSG 11.02.2021
     using System.Runtime.Serialization;
-    
-//    [System.Diagnostics.DebuggerStepThroughAttribute()]
-//    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
-//    [System.Runtime.Serialization.DataContractAttribute(Name="Acuse", Namespace="http://schemas.datacontract.org/2004/07/Sat.Cfdi.Negocio.ConsultaCfdi.Servicio")]
-//    public partial class Acuse : object, System.Runtime.Serialization.IExtensibleDataObject
-//    {
-        
-//        private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
-        
-//        private string CodigoEstatusField;
-        
-//        private string EstadoField;
-        
-//        public System.Runtime.Serialization.ExtensionDataObject ExtensionData
-//        {
-//            get
-//            {
-//                return this.extensionDataField;
-//            }
-//            set
-//            {
-//                this.extensionDataField = value;
-//            }
-//        }
-        
-//        [System.Runtime.Serialization.DataMemberAttribute()]
-//        public string CodigoEstatus
-//        {
-//            get
-//            {
-//                return this.CodigoEstatusField;
-//            }
-//            set
-//            {
-//                this.CodigoEstatusField = value;
-//            }
-//        }
-        
-//        [System.Runtime.Serialization.DataMemberAttribute()]
-//        public string Estado
-//        {
-//            get
-//            {
-//                return this.EstadoField;
-//            }
-//            set
-//            {
-//                this.EstadoField = value;
-//            }
-//        }
-//    }
+
+    //    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    //    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.Runtime.Serialization", "4.0.0.0")]
+    //    [System.Runtime.Serialization.DataContractAttribute(Name="Acuse", Namespace="http://schemas.datacontract.org/2004/07/Sat.Cfdi.Negocio.ConsultaCfdi.Servicio")]
+    //    public partial class Acuse : object, System.Runtime.Serialization.IExtensibleDataObject
+    //    {
+
+    //        private System.Runtime.Serialization.ExtensionDataObject extensionDataField;
+
+    //        private string CodigoEstatusField;
+
+    //        private string EstadoField;
+
+    //        public System.Runtime.Serialization.ExtensionDataObject ExtensionData
+    //        {
+    //            get
+    //            {
+    //                return this.extensionDataField;
+    //            }
+    //            set
+    //            {
+    //                this.extensionDataField = value;
+    //            }
+    //        }
+
+    //        [System.Runtime.Serialization.DataMemberAttribute()]
+    //        public string CodigoEstatus
+    //        {
+    //            get
+    //            {
+    //                return this.CodigoEstatusField;
+    //            }
+    //            set
+    //            {
+    //                this.CodigoEstatusField = value;
+    //            }
+    //        }
+
+    //        [System.Runtime.Serialization.DataMemberAttribute()]
+    //        public string Estado
+    //        {
+    //            get
+    //            {
+    //                return this.EstadoField;
+    //            }
+    //            set
+    //            {
+    //                this.EstadoField = value;
+    //            }
+    //        }
+    //    }
 
 
 
-//    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
-//    [System.ServiceModel.ServiceContractAttribute(ConfigurationName="IConsultaCFDIService")]
-//    public interface IConsultaCFDIService
-//    {
-    
-//        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IConsultaCFDIService/Consulta", ReplyAction="http://tempuri.org/IConsultaCFDIService/ConsultaResponse")]
-//        PEntidades.SrvSATConsultaCFDI.Acuse Consulta(string expresionImpresa);
-    
-//        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IConsultaCFDIService/Consulta", ReplyAction="http://tempuri.org/IConsultaCFDIService/ConsultaResponse")]
-//        System.Threading.Tasks.Task<PEntidades.SrvSATConsultaCFDI.Acuse> ConsultaAsync(string expresionImpresa);
-//    }
+    //    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    //    [System.ServiceModel.ServiceContractAttribute(ConfigurationName="IConsultaCFDIService")]
+    //    public interface IConsultaCFDIService
+    //    {
 
-//    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
-//    public interface IConsultaCFDIServiceChannel : IConsultaCFDIService, System.ServiceModel.IClientChannel
-//    {
-//    }
+    //        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IConsultaCFDIService/Consulta", ReplyAction="http://tempuri.org/IConsultaCFDIService/ConsultaResponse")]
+    //        PEntidades.SrvSATConsultaCFDI.Acuse Consulta(string expresionImpresa);
 
-//    [System.Diagnostics.DebuggerStepThroughAttribute()]
-//    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    //        [System.ServiceModel.OperationContractAttribute(Action="http://tempuri.org/IConsultaCFDIService/Consulta", ReplyAction="http://tempuri.org/IConsultaCFDIService/ConsultaResponse")]
+    //        System.Threading.Tasks.Task<PEntidades.SrvSATConsultaCFDI.Acuse> ConsultaAsync(string expresionImpresa);
+    //    }
 
-//    public partial class ConsultaCFDIServiceClient : System.ServiceModel.ClientBase<IConsultaCFDIService>, IConsultaCFDIService
-//    {
-    
-//        public ConsultaCFDIServiceClient()
-//        {
-//        }
-    
-//        public ConsultaCFDIServiceClient(string endpointConfigurationName) : 
-//                base(endpointConfigurationName)
-//        {
-//        }
-    
-//        public ConsultaCFDIServiceClient(string endpointConfigurationName, string remoteAddress) : 
-//                base(endpointConfigurationName, remoteAddress)
-//        {
-//        }
-    
-//        public ConsultaCFDIServiceClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) : 
-//                base(endpointConfigurationName, remoteAddress)
-//        {
-//        }
-    
-//        public ConsultaCFDIServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
-//                base(binding, remoteAddress)
-//        {
-//        }
+    //    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    //    public interface IConsultaCFDIServiceChannel : IConsultaCFDIService, System.ServiceModel.IClientChannel
+    //    {
+    //    }
 
-//        public PEntidades.SrvSATConsultaCFDI.Acuse Consulta(string expresionImpresa)
-//        {
-//            return base.Channel.Consulta(expresionImpresa);
-//        }
+    //    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    //    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
 
-//        public System.Threading.Tasks.Task<PEntidades.SrvSATConsultaCFDI.Acuse> ConsultaAsync(string expresionImpresa)
-//        {
-//            return base.Channel.ConsultaAsync(expresionImpresa);
-//        }
-//    }
+    //    public partial class ConsultaCFDIServiceClient : System.ServiceModel.ClientBase<IConsultaCFDIService>, IConsultaCFDIService
+    //    {
+
+    //        public ConsultaCFDIServiceClient()
+    //        {
+    //        }
+
+    //        public ConsultaCFDIServiceClient(string endpointConfigurationName) : 
+    //                base(endpointConfigurationName)
+    //        {
+    //        }
+
+    //        public ConsultaCFDIServiceClient(string endpointConfigurationName, string remoteAddress) : 
+    //                base(endpointConfigurationName, remoteAddress)
+    //        {
+    //        }
+
+    //        public ConsultaCFDIServiceClient(string endpointConfigurationName, System.ServiceModel.EndpointAddress remoteAddress) : 
+    //                base(endpointConfigurationName, remoteAddress)
+    //        {
+    //        }
+
+    //        public ConsultaCFDIServiceClient(System.ServiceModel.Channels.Binding binding, System.ServiceModel.EndpointAddress remoteAddress) : 
+    //                base(binding, remoteAddress)
+    //        {
+    //        }
+
+    //        public PEntidades.SrvSATConsultaCFDI.Acuse Consulta(string expresionImpresa)
+    //        {
+    //            return base.Channel.Consulta(expresionImpresa);
+    //        }
+
+    //        public System.Threading.Tasks.Task<PEntidades.SrvSATConsultaCFDI.Acuse> ConsultaAsync(string expresionImpresa)
+    //        {
+    //            return base.Channel.ConsultaAsync(expresionImpresa);
+    //        }
+    //    }
 
     public class ValidarCFDI
     {
@@ -138,12 +138,15 @@ namespace PEntidades.SrvSATConsultaCFDI
 
         public PEntidades.SrvSATConsultaCFDI.MiAcuse esValidoCFDI(string expresionImpresa)
         {
-            if (System.Net.ServicePointManager.SecurityProtocol == (SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls))//ADD SF RSG 11.02.2021
-                System.Net.ServicePointManager.SecurityProtocol = (SecurityProtocolType)(0xc0 | 0x300 | 0xc00);
+            //if (System.Net.ServicePointManager.SecurityProtocol == (SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls))      //ADD SF RSG 11.02.2021      //DELETE SF RSG 12.04.2021
+            //{
+            System.Net.ServicePointManager.SecurityProtocol = (SecurityProtocolType)(0xc0 | 0x300 | 0xc00);
+            System.Net.ServicePointManager.SecurityProtocol &= ~SecurityProtocolType.Ssl3;                                          //ADD SF RSG 12.04.2021
+            //}
             PEntidades.SrvSATConsultaCFDI.ConsultaCFDIServiceClient cli = new SrvSATConsultaCFDI.ConsultaCFDIServiceClient(
-                new System.ServiceModel.BasicHttpBinding("BasicHttpBinding_IConsultaCFDIService"), 
+                new System.ServiceModel.BasicHttpBinding("BasicHttpBinding_IConsultaCFDIService"),
                 new System.ServiceModel.EndpointAddress("https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc"));
-               // new System.ServiceModel.EndpointAddress("https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc"));
+            // new System.ServiceModel.EndpointAddress("https://consultaqr.facturaelectronica.sat.gob.mx/ConsultaCFDIService.svc"));
             cli.Open();
             PEntidades.SrvSATConsultaCFDI.Acuse resul = cli.Consulta(expresionImpresa);
             MiAcuse retorno = new MiAcuse();
@@ -157,7 +160,7 @@ namespace PEntidades.SrvSATConsultaCFDI
     {
         private string codigoEstatus;
         private string estado;
-        
+
         public MiAcuse()
         {
         }
@@ -173,6 +176,6 @@ namespace PEntidades.SrvSATConsultaCFDI
             get { return estado; }
             set { estado = value; }
         }
-        
+
     }
 }
